efbb61f15e815def63c5bdd269d512bb479783be
Paweł <pm4public@gmail.com>
2019-11-26 22:34:53 +0100

    MIGRATIONS reset

--- cmsplugin_filer_video/migrations/0001_initial.py
+++ cmsplugin_filer_video/migrations/0001_initial.py
@@ -7,6 +7,7 @@ import filer.fields.file
 from cmsplugin_filer_video import settings
 
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -18,18 +19,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FilerVideo',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE)),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.deletion.CASCADE)),
                 ('movie_url', models.CharField(help_text='vimeo or youtube video url. Example: http://www.youtube.com/watch?v=YFa59lK-kpo', max_length=255, null=True, verbose_name='movie url', blank=True)),
                 ('width', models.PositiveSmallIntegerField(verbose_name='width', default=settings.VIDEO_WIDTH)),
-                ('width_units', models.CharField(verbose_name="width units", max_length=2, default=settings.VIDEO_UNITS)),
                 ('height', models.PositiveSmallIntegerField(verbose_name='height', default=settings.VIDEO_HEIGHT)),
-                ('height_units', models.CharField(verbose_name="height units", max_length=2, default=settings.VIDEO_UNITS)),
                 ('auto_play', models.BooleanField(verbose_name='auto play', default=settings.VIDEO_AUTOPLAY)),
                 ('auto_hide', models.BooleanField(verbose_name='auto hide', default=settings.VIDEO_AUTOHIDE)),
-                ('controls', models.BooleanField(verbose_name='controls', default=settings.VIDEO_CONTROLS)),
-                ('muted', models.BooleanField(verbose_name='muted', default=settings.VIDEO_MUTED)),
+                ('fullscreen', models.BooleanField(verbose_name='fullscreen', default=0)),
                 ('loop', models.BooleanField(verbose_name='loop', default=settings.VIDEO_LOOP)),
-                ('preload', models.CharField(verbose_name='preload', default=settings.VIDEO_PRELOAD, max_length=16)),
                 ('bgcolor', models.CharField(default=settings.VIDEO_BG_COLOR, max_length=6, verbose_name='background color', help_text='Hexadecimal, eg ff00cc')),
                 ('textcolor', models.CharField(default=settings.VIDEO_TEXT_COLOR, max_length=6, verbose_name='text color', help_text='Hexadecimal, eg ff00cc')),
                 ('seekbarcolor', models.CharField(default=settings.VIDEO_SEEKBAR_COLOR, max_length=6, verbose_name='seekbar color', help_text='Hexadecimal, eg ff00cc')),
@@ -38,8 +35,8 @@ class Migration(migrations.Migration):
                 ('buttonoutcolor', models.CharField(default=settings.VIDEO_BUTTON_OUT_COLOR, max_length=6, verbose_name='button out color', help_text='Hexadecimal, eg ff00cc')),
                 ('buttonovercolor', models.CharField(default=settings.VIDEO_BUTTON_OVER_COLOR, max_length=6, verbose_name='button over color', help_text='Hexadecimal, eg ff00cc')),
                 ('buttonhighlightcolor', models.CharField(default=settings.VIDEO_BUTTON_HIGHLIGHT_COLOR, max_length=6, verbose_name='button highlight color', help_text='Hexadecimal, eg ff00cc')),
-                ('image', filer.fields.image.FilerImageField(null=True, to='filer.Image', help_text='preview image file', related_name='filer_video_image', blank=True, verbose_name='image', on_delete=models.SET_NULL)),
-                ('movie', filer.fields.file.FilerFileField(null=True, to='filer.File', help_text='use .flv file or h264 encoded video file', blank=True, verbose_name='movie file', on_delete=models.SET_NULL)),
+                ('image', filer.fields.image.FilerImageField(null=True, to='filer.Image', help_text='preview image file', related_name='filer_video_image', blank=True, verbose_name='image', on_delete=models.deletion.CASCADE)),
+                ('movie', filer.fields.file.FilerFileField(null=True, to='filer.File', help_text='use .flv file or h264 encoded video file', blank=True, verbose_name='movie file', on_delete=models.deletion.CASCADE)),
             ],
             options={
                 'abstract': False,
